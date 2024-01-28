1daffdd77e380efa7c247a8a1ddc8d840ad761c4
Aksil IRBOUH <aksil.irbouh@edu.devinci.fr>
2024-01-28 17:13:28 +0100

    First version of Calculator3000

---
+++ app/src/main/java/com/example/figma_aksil_irbouh/frame_activity.java
@@ -0,0 +1,152 @@
+package com.example.figma_aksil_irbouh;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		frame
+	 *	@date 		Monday 22nd of January 2024 05:29:01 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class frame_activity extends Activity {
+
+	
+	private View _bg__frame_ek2;
+	private View _bg__frame_23_ek1;
+	private TextView _0;
+	private View _bg__frame_20_ek1;
+	private TextView __;
+	private View _bg__frame_24_ek1;
+	private TextView ___ek1;
+	private View _bg__group_8_ek1;
+	private View rectangle_9;
+	private View rectangle_8;
+	private View _bg__frame_3_ek1;
+	private TextView _3;
+	private View _bg__frame_1_ek1;
+	private TextView _1;
+	private View _bg__frame_2_ek1;
+	private TextView _2;
+	private View _bg__frame_28_ek1;
+	private ImageView deg;
+	private View _bg__frame_27_ek1;
+	private ImageView sin;
+	private View _bg__frame_25_ek1;
+	private ImageView e;
+	private View _bg__frame_26_ek1;
+	private ImageView mu;
+	private View _bg__frame_8_ek1;
+	private TextView ___ek2;
+	private View _bg__frame_13_ek1;
+	private TextView _9;
+	private View _bg__frame_9_ek1;
+	private TextView _7;
+	private View _bg__frame_11_ek1;
+	private TextView _8;
+	private View _bg__frame_7_ek1;
+	private TextView _6;
+	private View _bg__frame_5_ek1;
+	private TextView _4;
+	private View _bg__frame_6_ek1;
+	private TextView _5;
+	private View _bg__group_3_ek1;
+	private View rectangle_10;
+	private ImageView ___ek3;
+	private View _bg__group_2_ek1;
+	private View rectangle_11;
+	private ImageView ___ek4;
+	private View _bg__frame_22_ek1;
+	private TextView ac;
+	private View _bg__group_1_ek1;
+	private View rectangle_12;
+	private ImageView vector;
+	private TextView __12_454;
+	private TextView _6000_2_3227_2;
+	private View _bg__frame_20_ek3;
+	private TextView ___ek5;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frame);
+
+		
+		_bg__frame_ek2 = (View) findViewById(R.id._bg__frame_ek2);
+		_bg__frame_23_ek1 = (View) findViewById(R.id._bg__frame_23_ek1);
+		_0 = (TextView) findViewById(R.id._0);
+		_bg__frame_20_ek1 = (View) findViewById(R.id._bg__frame_20_ek1);
+		__ = (TextView) findViewById(R.id.__);
+		_bg__frame_24_ek1 = (View) findViewById(R.id._bg__frame_24_ek1);
+		___ek1 = (TextView) findViewById(R.id.___ek1);
+		_bg__group_8_ek1 = (View) findViewById(R.id._bg__group_8_ek1);
+		rectangle_9 = (View) findViewById(R.id.rectangle_9);
+		rectangle_8 = (View) findViewById(R.id.rectangle_8);
+		_bg__frame_3_ek1 = (View) findViewById(R.id._bg__frame_3_ek1);
+		_3 = (TextView) findViewById(R.id._3);
+		_bg__frame_1_ek1 = (View) findViewById(R.id._bg__frame_1_ek1);
+		_1 = (TextView) findViewById(R.id._1);
+		_bg__frame_2_ek1 = (View) findViewById(R.id._bg__frame_2_ek1);
+		_2 = (TextView) findViewById(R.id._2);
+		_bg__frame_28_ek1 = (View) findViewById(R.id._bg__frame_28_ek1);
+		deg = (ImageView) findViewById(R.id.deg);
+		_bg__frame_27_ek1 = (View) findViewById(R.id._bg__frame_27_ek1);
+		sin = (ImageView) findViewById(R.id.sin);
+		_bg__frame_25_ek1 = (View) findViewById(R.id._bg__frame_25_ek1);
+		e = (ImageView) findViewById(R.id.e);
+		_bg__frame_26_ek1 = (View) findViewById(R.id._bg__frame_26_ek1);
+		mu = (ImageView) findViewById(R.id.mu);
+		_bg__frame_8_ek1 = (View) findViewById(R.id._bg__frame_8_ek1);
+		___ek2 = (TextView) findViewById(R.id.___ek2);
+		_bg__frame_13_ek1 = (View) findViewById(R.id._bg__frame_13_ek1);
+		_9 = (TextView) findViewById(R.id._9);
+		_bg__frame_9_ek1 = (View) findViewById(R.id._bg__frame_9_ek1);
+		_7 = (TextView) findViewById(R.id._7);
+		_bg__frame_11_ek1 = (View) findViewById(R.id._bg__frame_11_ek1);
+		_8 = (TextView) findViewById(R.id._8);
+		_bg__frame_7_ek1 = (View) findViewById(R.id._bg__frame_7_ek1);
+		_6 = (TextView) findViewById(R.id._6);
+		_bg__frame_5_ek1 = (View) findViewById(R.id._bg__frame_5_ek1);
+		_4 = (TextView) findViewById(R.id._4);
+		_bg__frame_6_ek1 = (View) findViewById(R.id._bg__frame_6_ek1);
+		_5 = (TextView) findViewById(R.id._5);
+		_bg__group_3_ek1 = (View) findViewById(R.id._bg__group_3_ek1);
+		rectangle_10 = (View) findViewById(R.id.rectangle_10);
+		___ek3 = (ImageView) findViewById(R.id.___ek3);
+		_bg__group_2_ek1 = (View) findViewById(R.id._bg__group_2_ek1);
+		rectangle_11 = (View) findViewById(R.id.rectangle_11);
+		___ek4 = (ImageView) findViewById(R.id.___ek4);
+		_bg__frame_22_ek1 = (View) findViewById(R.id._bg__frame_22_ek1);
+		ac = (TextView) findViewById(R.id.ac);
+		_bg__group_1_ek1 = (View) findViewById(R.id._bg__group_1_ek1);
+		rectangle_12 = (View) findViewById(R.id.rectangle_12);
+		vector = (ImageView) findViewById(R.id.vector);
+		__12_454 = (TextView) findViewById(R.id.__12_454);
+		_6000_2_3227_2 = (TextView) findViewById(R.id._6000_2_3227_2);
+		_bg__frame_20_ek3 = (View) findViewById(R.id._bg__frame_20_ek3);
+		___ek5 = (TextView) findViewById(R.id.___ek5);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
